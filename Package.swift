ffc663f3c8606a43a78a55cde2f9e22019c1263d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-09 17:29:00 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-affine-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "Affine Primitives",
+            targets: ["Affine Primitives"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../swift-algebra-linear-primitives"),
+        .package(path: "../swift-formatting-primitives"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
+        .package(path: "../swift-test-support-primitives"),
+    ],
+    targets: [
+        .target(
+            name: "Affine Primitives",
+            dependencies: [
+                .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
+                .product(name: "Formatting Primitives", package: "swift-formatting-primitives"),
+                .product(name: "RealModule", package: "swift-numerics"),
+            ]
+        ),
+        .testTarget(
+            name: "Affine Primitives Tests",
+            dependencies: [
+                "Affine Primitives",
+                .product(name: "Test Support Primitives", package: "swift-test-support-primitives"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
