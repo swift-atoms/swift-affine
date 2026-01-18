6204e98a808081541faf4aef1a53caf6b808b14c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 20:13:54 +0100

    Migrate tests to nested package pattern

--- Package.swift
+++ Package.swift
@@ -9,19 +9,18 @@ let package = Package(
         .iOS(.v26),
         .tvOS(.v26),
         .watchOS(.v26),
-        .visionOS(.v26),
+        .visionOS(.v26)
     ],
     products: [
         .library(
             name: "Affine Primitives",
             targets: ["Affine Primitives"]
-        ),
+        )
     ],
     dependencies: [
         .package(path: "../swift-algebra-linear-primitives"),
         .package(path: "../swift-formatting-primitives"),
-        .package(path: "../swift-numeric-primitives"),
-        .package(path: "../swift-test-primitives"),
+        .package(path: "../swift-numeric-primitives")
     ],
     targets: [
         .target(
@@ -29,16 +28,9 @@ let package = Package(
             dependencies: [
                 .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
                 .product(name: "Formatting Primitives", package: "swift-formatting-primitives"),
-                .product(name: "Real Primitives", package: "swift-numeric-primitives"),
+                .product(name: "Real Primitives", package: "swift-numeric-primitives")
             ]
-        ),
-        .testTarget(
-            name: "Affine Primitives Tests",
-            dependencies: [
-                "Affine Primitives",
-                .product(name: "Test Primitives", package: "swift-test-primitives"),
-            ]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
@@ -49,7 +41,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
         .enableExperimentalFeature("Lifetimes"),
-        .strictMemorySafety(),
+        .strictMemorySafety()
     ]
     target.swiftSettings = (target.swiftSettings ?? []) + settings
 }
